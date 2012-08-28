0def3364288937de0561e4ab5ac8fc7f4e899056
Ryan LaBouve <imryanlabouve@gmail.com>
2012-08-27 21:34:49 -0500

    Reorganize setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# TODO: initialize git sub-modules and test out plugins
+# Create vim-tmp for swap files
+mkdir -p ~/.vim-tmp
+
+# make symbolic links from system dotfiles to repo
+REPO_DIR=`pwd`
+echo "Setting $REPO_DIR/vimrc to ~/.test"
+ln -nfs $REPO_DIR/vimrc ~/.vimrc
+ln -fs $REPO_DIR/vim ~/.vim
+ln -nfs $REPO_DIR/zshrc ~/.zshrc
+ln -fs $REPO_DIR/zsh-modules ~/.zsh-modules
 
 
-# make symbolic link for rcs
-CURR_DIR=`pwd`
-echo "Setting $CURR_DIR/vimrc to ~/.test"
-ln -nfs $CURR_DIR/vimrc ~/.vimrc
-ln -fs $CURR_DIR/vim ~/.vim
-ln -nfs $CURR_DIR/zshrc ~/.zshrc
-ln -fs $CURR_DIR/zsh-modules ~/.zsh-modules
+# copy required bins and set permissions
+mkdir -p ~/bin
+cp bin/* ~/bin/
+chmod +x ~/bin/git-current-branch
+chmod +x ~/bin/git-cwd-info 
 
-echo `pwd`/vim
-# make terminal open zsh on login
+# make terminal open zsh on bash login
 HAS_ZSH=(`grep -c "zsh" ~/.bash_profile`)
 if [ $HAS_ZSH -lt 1 ]; then
 	# $HAS_ZSH is the number of time zsh occurs in bash_profile
@@ -23,13 +29,6 @@ else
 	echo "zsh already loaded through bash_profile"
 fi
 
-# copy required bins and set permissions
-mkdir -p ~/bin
-cp bin/* ~/bin/
-chmod +x ~/bin/git-current-branch
-chmod +x ~/bin/git-cwd-info 
-
-mkdir -p ~/.vim-tmp
 # Git Stuff
 # http://news.ycombinator.com/item?id=4130494
 git config --global color.ui "auto"
